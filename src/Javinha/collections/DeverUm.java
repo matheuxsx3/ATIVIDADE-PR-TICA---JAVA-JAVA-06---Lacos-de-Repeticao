77c5c1ac1ae042168970639dd190504a5b1fa0a2
Matheus Pereira da Silva <matheus.s280604@gmail.com>
2023-09-04 23:19:17 -0300

    atividade collections

--- src/Javinha/collections/DeverUm.java
+++ src/Javinha/collections/DeverUm.java
@@ -1,3 +1,8 @@
+//Escreva um programa Java para criar uma Collection ArrayList de Objetos da Classe String.
+// O programa deverá solicitar ao usuário, que ele digite via teclado 5 cores
+// e deverá adicioná-las individualmente no ArrayList. Em seguida, faça o que se pede:
+//Mostre na tela todas as cores que foram adicionadas.
+//Mostre na tela todas as cores que foram adicionadas ordenadas em ordem crescente.
 package Javinha.collections;
 
 import java.util.ArrayList;
