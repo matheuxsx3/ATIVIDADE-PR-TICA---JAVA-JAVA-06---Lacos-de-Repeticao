42342960109a063c5d5dcc829353f9c3e9c72e36
Matheus Pereira da Silva <matheus.s280604@gmail.com>
2023-09-05 00:36:59 -0300

    atividade collections

---
+++ src/Javinha/collections/DeverTres.java
@@ -0,0 +1,32 @@
+//Escreva um programa Java para criar uma Collection Set de Objetos da Classe Wrapper Integer.
+// O programa deverá solicitar ao usuário, que ele digite via teclado 10 valores inteiros não repetidos
+// e adicione-os individualmente na Collection Set. Em seguida, faça o que se pede:
+//Mostre na tela todos os elementos da Collection Set, utilizando a Classe Iterator
+
+package Javinha.collections;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Scanner;
+import java.util.Set;
+
+public class DeverTres {
+    public static void main(String[] args) {
+        Scanner leia = new Scanner(System.in);
+        Set<Integer> numeros = new HashSet<>();
+        int num;
+        System.out.println("Você deve entrar com 10 numeros inteiros:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Digite um número inteiro:");
+            num = leia.nextInt();
+            numeros.add(num);
+        }
+        System.out.println("Listar dados do Set:");
+
+        Iterator<Integer> inumeros = numeros.iterator();
+
+        while (inumeros.hasNext()) {
+            System.out.println(inumeros.next());
+        }
+    }
+}
