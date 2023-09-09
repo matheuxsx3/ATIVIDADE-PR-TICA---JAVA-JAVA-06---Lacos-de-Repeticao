b8ae618124b2bcfbe0b8f3f038652063d6090e4c
Matheus Pereira da Silva <matheus.s280604@gmail.com>
2023-09-08 22:34:15 -0300

    atualização

--- src/Javinha/funcao/DeverUm.java
+++ src/Javinha/funcao/DeverUm.java
@@ -2,9 +2,7 @@ package Javinha.funcao;
 
 public class DeverUm {
     public static void main(String[] args) {
-        System.out.println("ola");
-
-
-        }
+        System.out.println("fase de teste");
     }
+}
 
