a5e8558b2c6351620e6013b38192554918ca7107
Matheus Pereira da Silva <matheus.s280604@gmail.com>
2023-09-11 14:56:47 -0300

    atualização

--- src/Javinha/LacosDecisao/IfElse/DeverUm.java
+++ src/Javinha/LacosDecisao/IfElse/DeverUm.java
@@ -2,6 +2,6 @@ package Javinha.LacosDecisao.IfElse;
 
 public class DeverUm {
     public static void main(String[] args) {
-
+        System.out.println("fase de teste");
     }
 }
--- src/Javinha/LacosDecisao/SwichCase/DeverUm.java
+++ src/Javinha/LacosDecisao/SwichCase/DeverUm.java
@@ -2,6 +2,6 @@ package Javinha.LacosDecisao.SwichCase;
 
 public class DeverUm {
     public static void main(String[] args) {
-
+        System.out.println("fase de teste");
     }
 }
