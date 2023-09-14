85cfa5e4751a3ddda199d8bcf00923c38cb3604d
Matheus Pereira da Silva <matheus.s280604@gmail.com>
2023-09-14 15:31:57 -0300

    atualização inteface e classes abstratas

--- src/Javinha/Poo/InterfacesAbstracao/InvalidCaseException.java
+++ src/Javinha/Poo/InterfacesAbstracao/InvalidCaseException.java
@@ -1,7 +1,6 @@
 package Javinha.Poo.InterfacesAbstracao;
 
 public class InvalidCaseException extends Exception {
-    // Construtor personalizado para a exceção
     public InvalidCaseException(String message) {
         super(message);
     }
